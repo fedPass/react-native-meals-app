6fc1928a1be2b1bd773e836249c1d7be69a8f90d
Federica Passanante <federica.passanante@sideagroup.com>
2024-05-20 11:21:31 +0200

    Pass data between screens

--- screens/CategoriesScreen.tsx
+++ screens/CategoriesScreen.tsx
@@ -7,15 +7,16 @@ import CategoryBox from '../components/CategoryBox';
 // alternately it's possible to import { useNavigation } from '@react-navigation/native'
 // and inside component declare const navigation = useNavigation()
 export default function CategoriesScreen({navigation}: any) {
-  const pressHandler = () => {
-    navigation.navigate('MealOverview');
+  const pressHandler = (id: string) => {
+    // navigation-navigate('name-of-screen', {params})
+    navigation.navigate('MealOverview', {id});
   }
 
   return (
     <View>
       <FlatList
         data={CATEGORIES}
-        renderItem={item => <CategoryBox item={item.item} onPress={pressHandler} /> }
+        renderItem={item => <CategoryBox item={item.item} onPress={() => pressHandler(item.item.id)} /> }
         keyExtractor={item => item.id}
         numColumns={2}
       />
--- screens/MealsOverviewScreen.tsx
+++ screens/MealsOverviewScreen.tsx
@@ -1,11 +1,15 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 
 import { MEALS } from "../data/dummy-data";
 
-export default function MealsOverviewScreen() {
+// beside navigation, we have another param that is route and contains info about current route, suchas params
+// we can also use import {useRoute } from '@react-navigation/native for take route info
+export default function MealsOverviewScreen({route}:any) {
+  const catId = route.params.id;
+  const displayedMeals = MEALS.filter(meal => { return meal.categoryIds.includes(catId)})
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen</Text>
+      <FlatList data={displayedMeals} renderItem={item => <Text>{item.item.title}</Text>}/>
     </View>
   )
 }
