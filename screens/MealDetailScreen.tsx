e26c30844c4409d2ad9ef654fe40aa2484703046
Federica Passanante <federica.passanante@sideagroup.com>
2024-05-20 15:45:45 +0200

    style fix

--- screens/MealDetailScreen.tsx
+++ screens/MealDetailScreen.tsx
@@ -1,71 +1,77 @@
-import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import {Image, ScrollView, StyleSheet, Text, View} from 'react-native';
 
 export default function MealDetailScreen({route}: any) {
   const mealInfo = route.params.item;
   return (
     <ScrollView>
+      <Image source={{uri: mealInfo.item.imageUrl}} style={styles.mealImage} />
       <View style={styles.mealContainer}>
         <Text style={styles.mealTitle}>{mealInfo.item.title}</Text>
-        <Image source={{uri: mealInfo.item.imageUrl}} style={styles.mealImage}/>
         <View style={styles.mealDetails}>
           <Text style={styles.mealDetailText}>
             Duration: {mealInfo.item.duration} min
-          </Text>      
+          </Text>
           <Text style={styles.mealDetailText}>
             Complexity: {mealInfo.item.complexity}
-          </Text>      
+          </Text>
           <Text style={styles.mealDetailText}>
             Affordability: {mealInfo.item.affordability}
           </Text>
         </View>
         <View style={styles.extraInfo}>
           <Text style={styles.extraInfoTitle}>Ingredients</Text>
-          {mealInfo.item.ingredients.map((ingredient:string) => <Text>- {ingredient}</Text>)}
+          {mealInfo.item.ingredients.map((ingredient: string) => (
+            <Text>- {ingredient}</Text>
+          ))}
         </View>
         <View style={styles.extraInfo}>
           <Text style={styles.extraInfoTitle}>Steps</Text>
-          {mealInfo.item.steps.map((step:string, index: number) => <Text>{index + 1}{')'} {step}</Text>)}
+          {mealInfo.item.steps.map((step: string, index: number) => (
+            <Text>
+              {index + 1}
+              {')'} {step}
+            </Text>
+          ))}
         </View>
       </View>
     </ScrollView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   mealContainer: {
     flex: 1,
-    margin: 16
+    margin: 16,
   },
   mealTitle: {
     fontSize: 18,
     fontWeight: 'bold',
     textAlign: 'center',
-    marginBottom: 12,
-    color: '#fff'
+    color: '#fff',
   },
   mealImage: {
     width: '100%',
-    height: 200
+    height: 300,
   },
   mealDetails: {
     marginVertical: 16,
-    alignItems: 'center'
+    alignItems: 'center',
   },
   mealDetailText: {
-    fontWeight: 'bold'
+    fontWeight: 'bold',
   },
   text: {
-    fontSize: 12
+    fontSize: 12,
   },
   extraInfo: {
     marginVertical: 12,
     padding: 8,
     borderRadius: 8,
-    backgroundColor: '#71B8BB'
+    backgroundColor: '#71B8BB',
   },
   extraInfoTitle: {
     textAlign: 'center',
     fontWeight: 'bold',
-    marginBottom: 8
-  }
+    marginBottom: 8,
+  },
 });
