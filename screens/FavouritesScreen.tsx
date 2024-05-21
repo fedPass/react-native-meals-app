ec4cb9e1453f188101dbf6db248475b49a3e120b
Federica Passanante <federica.passanante@sideagroup.com>
2024-05-21 19:07:07 +0200

    managed favouritesScreen

--- screens/FavouritesScreen.tsx
+++ screens/FavouritesScreen.tsx
@@ -1,7 +1,47 @@
-import { Text } from "react-native";
+import {FlatList, StyleSheet, Text, View} from 'react-native';
+import {useSelector} from 'react-redux';
+import {MEALS} from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 export default function FavouritesScreen() {
-  return(
-    <Text>Your favourites meals</Text>
+  const favouriteMealIds = useSelector(
+    (state: any) => state.favouriteMeals.ids,
+  );
+
+  const favouriteMeals = MEALS.filter(meal =>
+    favouriteMealIds.includes(meal.id),
+  );
+
+  if (favouriteMeals.length) {    
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={favouriteMeals}
+          renderItem={item => <MealItem item={item} />}
+        />
+      </View>
+    );
+  }
+  return (
+    <View style={styles.empty}>
+      <Text style={styles.emptyText}>No favourite meals yet</Text>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  empty: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  emptyText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 18
+  }
+});
